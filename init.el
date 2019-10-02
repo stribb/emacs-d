b8a1f9d2887c053cc2e4073478ca4d1a4f5c39e4
Andrew Stribblehill <stribb@gmail.com>
2019-10-02 20:37:44 +0200

    Python: completions, mypy, py-isort

--- init.el
+++ init.el
@@ -436,6 +436,32 @@
     :config (add-hook 'go-mode-hook 'go-eldoc-setup)))
 
 (progn
+  (with-eval-after-load 'python
+    (defun python-shell-completion-native-try ()
+      "Return non-nil if can trigger native completion."
+      (let ((python-shell-completion-native-enable t)
+            (python-shell-completion-native-output-timeout
+             python-shell-completion-native-try-output-timeout))
+        (python-shell-completion-native-get-completions
+         (get-buffer-process (current-buffer))
+         nil "_"))))
+
+  (flycheck-define-checker
+      python-mypy ""
+      :command ("mypy"
+                "--ignore-missing-imports"
+                "--python-version" "3.6"
+                source-original)
+      :error-patterns
+      ((error line-start (file-name) ":" line ": error:" (message) line-end))
+      :modes python-mode)
+  (add-to-list 'flycheck-checkers 'python-mypy t)
+  (flycheck-add-next-checker 'python-pylint 'python-mypy t)
+
+  (use-package py-isort
+    :after python
+    :config (add-hook 'before-save-hook 'py-isort-before-save))
+
   (use-package elpy
     :after python flycheck
     :config
@@ -446,12 +472,8 @@
 
   (use-package blacken
     :after python
-    :config (add-hook 'elpy-mode-hook 'blacken-mode))
+    :config (add-hook 'elpy-mode-hook 'blacken-mode)))
 
-  (use-package py-autopep8
-    :disabled  ;; experimentally
-    :after python
-    :config (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)))
 
 (use-package groovy-mode
   :mode "\\.groovy\\'")
