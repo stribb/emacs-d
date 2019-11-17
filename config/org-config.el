ecd019d793f0b5a5d5002ec13ef6f43077019b4c
Andrew Stribblehill <stribb@gmail.com>
2019-11-17 03:36:14 +0100

    Add markdown export to org-mode.

--- config/org-config.el
+++ config/org-config.el
@@ -15,6 +15,7 @@
 
 (setq-default org-confirm-babel-evaluate nil)
 
+(setq-default org-export-backends '(ascii html latex md))
 ;; Experimenting with visual-line-mode for text-modes.
 ;; (add-hook 'org-mode-hook #'turn-on-auto-fill)
 
