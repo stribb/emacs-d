6a94259edc8f8466c124d3bcda0b1a987f0deb90
Andrew Stribblehill <stribb@gmail.com>
2020-09-23 17:14:44 +0200

    When magit visits a smergable file, start Hydra.
    Thanks alphapapa for the Hydra.

--- init.el
+++ init.el
@@ -226,6 +226,47 @@
         solarized-high-contrast-mode-line t)
   (load-theme 'solarized-light t))
 
+;; https://github.com/alphapapa/unpackaged.el#hydra
+(use-package smerge-mode
+  :after hydra
+  :config
+  (defhydra alphapapa/smerge-hydra
+    (:color pink :hint nil :post (smerge-auto-leave))
+    "
+^Move^       ^Keep^               ^Diff^                 ^Other^
+^^-----------^^-------------------^^---------------------^^-------
+_n_ext       _b_ase               _<_: upper/base        _C_ombine
+_p_rev       _u_pper              _=_: upper/lower       _r_esolve
+^^           _l_ower              _>_: base/lower        _k_ill current
+^^           _a_ll                _R_efine
+^^           _RET_: current       _E_diff
+"
+    ("n" smerge-next)
+    ("p" smerge-prev)
+    ("b" smerge-keep-base)
+    ("u" smerge-keep-upper)
+    ("l" smerge-keep-lower)
+    ("a" smerge-keep-all)
+    ("RET" smerge-keep-current)
+    ("\C-m" smerge-keep-current)
+    ("<" smerge-diff-base-upper)
+    ("=" smerge-diff-upper-lower)
+    (">" smerge-diff-base-lower)
+    ("R" smerge-refine)
+    ("E" smerge-ediff)
+    ("C" smerge-combine-with-next)
+    ("r" smerge-resolve)
+    ("k" smerge-kill-current)
+    ("ZZ" (lambda ()
+            (interactive)
+            (save-buffer)
+            (bury-buffer))
+     "Save and bury buffer" :color blue)
+    ("q" nil "cancel" :color blue))
+  :hook (magit-diff-visit-file-hook . (lambda ()
+                                        (when smerge-mode
+                                          (alphapapa/smerge-hydra/body)))))
+
 (use-package magit
   ;; Work around https://github.com/dgutov/diff-hl/issues/65 -- let's see if it works again?
   ;; :init (global-auto-revert-mode)
