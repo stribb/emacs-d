7caf2856e2a7d6adf42fc81a6c5b6d3b53b5968e
Andrew Stribblehill <stribb@arista.com>
2024-11-21 14:08:56 +0000

    custom.el changes

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("0598c6a29e13e7112cfbc2f523e31927ab7dce56ebb2016b567e1eff6dc1fd4f" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))
+   '("4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" "d89e15a34261019eec9072575d8a924185c27d3da64899905f8548cbd9491a36" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "0598c6a29e13e7112cfbc2f523e31927ab7dce56ebb2016b567e1eff6dc1fd4f" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))
  '(safe-local-variable-values
    '((projectile-project-type quote python-pipenv)
      (projectile-project-test-cmd . "pytest")
@@ -28,6 +28,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((((class color) (min-colors 89)) (:foreground "#657b83" :background "#fdf6e3"))))
+ '(helm-locate-finish ((t (:extend t :foreground "#859900\11"))))
  '(mode-line ((((class color) (min-colors 89)) (:inverse-video unspecified :overline "#657b83" :underline nil :foreground "#586e75" :background "#eee8d5" :box (:line-width (1 . 1) :color "#dc322f" :style unspecified))))))
 (put 'narrow-to-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
