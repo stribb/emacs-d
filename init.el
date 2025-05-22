62a44e07bd3002b42a2b80f8abea6fc39d750d94
stribb <stribb@arista.com>
2025-05-22 15:49:44 +0100

    Move Python config to config/python-config

--- init.el
+++ init.el
@@ -579,46 +579,7 @@ _p_rev       _u_pper              _=_: upper/lower       _r_esolve
                :host github
                :repo "gleam-lang/gleam-mode")))
 
-(progn
-  (with-eval-after-load 'python
-    (defun python-shell-completion-native-try ()
-      "Return non-nil if can trigger native completion."
-      (let ((python-shell-completion-native-enable t)
-            (python-shell-completion-native-output-timeout
-             python-shell-completion-native-try-output-timeout))
-        (python-shell-completion-native-get-completions
-         (get-buffer-process (current-buffer))
-         nil "_"))))
-
-  (flycheck-define-checker
-      python-mypy ""
-      :command ("mypy"
-                "--ignore-missing-imports"
-                "--python-version" "3.6"
-                source-original)
-      :error-patterns
-      ((error line-start (file-name) ":" line ": error:" (message) line-end))
-      :modes python-mode)
-  (add-to-list 'flycheck-checkers 'python-mypy t)
-  (flycheck-add-next-checker 'python-pylint 'python-mypy t)
-
-  (use-package py-isort
-    :after python
-    :config (add-hook 'before-save-hook 'py-isort-before-save))
-
-  (use-package elpy
-    :after python flycheck
-    :config
-    (elpy-enable)
-    ;; flycheck > flymake
-    (setq elpy-modules (delq 'elpy-module-flymake elpy-modules)
-          elpy-rpc-python-command "/home/stribb/.pyenv/shims/python")
-    (add-hook 'elpy-mode-hook 'flycheck-mode))
-
-  (use-package blacken
-    :after python
-    :config (add-hook 'elpy-mode-hook 'blacken-mode)))
-
+(require 'python-config)
 
 (use-package groovy-mode
   :mode "\\.groovy\\'")
