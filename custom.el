00aba1353788b2ebc1a18d1d9d4d15c464598916
stribb <stribb@arista.com>
2026-02-06 16:30:10 +0000

    Update custom

--- custom.el
+++ custom.el
@@ -6,22 +6,38 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" "d89e15a34261019eec9072575d8a924185c27d3da64899905f8548cbd9491a36" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "0598c6a29e13e7112cfbc2f523e31927ab7dce56ebb2016b567e1eff6dc1fd4f" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))
+   '("4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3"
+     "d89e15a34261019eec9072575d8a924185c27d3da64899905f8548cbd9491a36"
+     "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c"
+     "0598c6a29e13e7112cfbc2f523e31927ab7dce56ebb2016b567e1eff6dc1fd4f"
+     "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0"
+     "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879"
+     "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4"
+     default))
  '(safe-local-variable-values
-   '((projectile-project-type quote python-pipenv)
+   '((eval setq-local eglot-workspace-configuration
+	   `
+	   ((:basedpyright :analysis
+			   (:autoSearchPaths t :diagnosticMode
+					     "openFilesOnly"
+					     :typeCheckingMode
+					     "standard"
+					     :useLibraryCodeForTypes t))))
+     (projectile-project-type quote python-pipenv)
      (projectile-project-test-cmd . "pytest")
-     (highlight-80+-columns . 100)
-     (checkdoc-package-keywords-flag)
+     (highlight-80+-columns . 100) (checkdoc-package-keywords-flag)
      (eval font-lock-add-keywords nil
-           `((,(concat "("
-                       (regexp-opt
-                        '("sp-do-move-op" "sp-do-move-cl" "sp-do-put-op" "sp-do-put-cl" "sp-do-del-op" "sp-do-del-cl")
-                        t)
-                       "\\_>")
-              1 'font-lock-variable-name-face)))
+	   `
+	   ((,(concat "("
+		      (regexp-opt
+		       '("sp-do-move-op" "sp-do-move-cl"
+			 "sp-do-put-op" "sp-do-put-cl" "sp-do-del-op"
+			 "sp-do-del-cl")
+		       t)
+		      "\\_>")
+	     1 'font-lock-variable-name-face)))
      (eval when buffer-file-name
-           (setq-local view-no-disable-on-exit t)
-           (view-mode-enter))))
+	   (setq-local view-no-disable-on-exit t) (view-mode-enter))))
  '(show-paren-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
