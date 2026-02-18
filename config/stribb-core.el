32eb1f4b925a215f3e32467c53cf9a5bc8078f1c
stribb <stribb@arista.com>
2026-02-18 16:14:54 +0000

    eshell: consolidate duplicate use-package blocks into one
    The 667d9b7 "fix eshell" commit left two conflicting use-package eshell
    blocks. Merge them: use default-directory (not eshell/pwd) for the path,
    and colourise by exit status via foreground colour on the whole path.

--- config/stribb-core.el
+++ config/stribb-core.el
@@ -905,28 +905,13 @@ With ARG, go ARG forward or backward."
   :straight nil
   :init
   (defun stribb/eshell-prompt-function ()
-    "Custom prompt function."
+    "Eshell prompt showing cwd, coloured by last exit status."
     (let ((success? (eq eshell-last-command-status 0)))
       (concat
-       (propertize ": " 'face `(:background ,(if success? "gray" "red")))
-       (abbreviate-file-name (eshell/pwd))
+       (propertize (abbreviate-file-name default-directory)
+                   'face `(:foreground ,(if success? "green" "red")))
        (if (= (user-uid) 0) " # " " ; "))))
   :config
-  (setq eshell-prompt-regexp "^: [^#$;\n]* [#$;] "
-        eshell-prompt-function #'stribb/eshell-prompt-function
-        eshell-hist-ignoredups t))
-
-(use-package eshell
-  :straight nil
-  :init
-  (defun stribb/eshell-prompt-function ()
-    "A working prompt function that doesn't crash."
-    ;; Use `default-directory` instead of `eshell/pwd`
-    (concat
-     (propertize (abbreviate-file-name default-directory) 'face '(:foreground "green"))
-     (if (= (user-uid) 0) " # " " ; ")))
-  :config
-  ;; Simplified regex to match the simplified prompt
   (setq eshell-prompt-regexp "^[^#;\n]* [#;] "
         eshell-prompt-function #'stribb/eshell-prompt-function
         eshell-hist-ignoredups t))
